bcf806155849eae1b27de7670e198c2afda92fa0
Marc LeBlanc <marc@leblanchq.com>
2020-05-28 12:32:11 -0400

    update module version

--- gke.tf
+++ gke.tf
@@ -1,6 +1,6 @@
 module "gke" {
   source  = "app.terraform.io/leblanchq/gke/google"
-  version = "1.0.0"
+  version = "1.1.0"
   cluster_name = var.cluster_name
   project_name = var.project_name
 }
